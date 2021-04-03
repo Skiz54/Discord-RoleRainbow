4706ebec4fbf0c6a41962cdfdeea4b40325b832d
Skiz54 <skizcontact0@gmail.com>
2021-04-03 11:45:06 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,39 @@
+# Discord-RoleRainbow 🏳️‍🌈
+Discord-RoleRainbow is a Discord bot that allows to get a rainbow role on your server
+
+⚠️ **Does not support multi-server !**
+
+## Setup
+
+* Clone the repository
+* Installing dependencies with NPM `npm install`
+* Modify the file *config.json*
+* Start the bot `node index.js` 
+
+## Setting
+
+🎨 Color 100% customizable ! 
+
+⚠️ If you are using a speed that's lower than 60000ms, your bot might get IP-banned !
+
+### Config.json
+```json
+{
+ "token":"YOUR_PREFIX",
+ "server_ID":"ID_SERVEUR",
+ "role_ID":"LOGCHANNEL_SERVEUR",
+ "speed":"60000",
+ "color":[
+  "#FF0505",
+  "#FFB200",
+  "#FFFB00",
+  "#B6FF00",
+  "#3AFF00",
+  "#00FFD1",
+  "#00A2FF",
+  "#0800FF",
+  "#BD00FF",
+  "#FF00EC"
+ ]
+}
+```
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js");
-const fs = require("fs");
 const client = new Discord.Client();
 
 
-let config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+let config = require("./config.json");
 client.config = config;
 
 let now = new Date();
@@ -14,24 +13,26 @@ let time = (`[${hour}:${minute}:${second}] | `);
 
 client.on('ready', () => {
 
-    console.log(time + `\x1b[32m%s\x1b[0m`, '[ OK ]', '\x1b[0m', 'Connexion à l\'API Discord.js effectuée');
-    console.log(time + `\x1b[36m%s\x1b[0m`, '[INFO]', '\x1b[0m', 'Connecté sur ' + client.user.username + '#' + client.user.discriminator);
+    console.log(time + `\x1b[32m%s\x1b[0m`, '[ OK ]', '\x1b[0m', 'Connection to the Discord.js API ');
+    console.log(time + `\x1b[36m%s\x1b[0m`, '[INFO]', '\x1b[0m', 'Connected to ' + client.user.username + '#' + client.user.discriminator);
 
-    client.user.setActivity("Discord-RoleRainbow", {
-     type: "PLAYING",
-    });
+    client.user.setActivity("Discord-RoleRainbow", {type: "PLAYING"});
 
     if (config.speed < 60000) {
-     console.log(time +`\x1b[33m%s\x1b[0m`, '[WARNING]', '\x1b[0m', 'The minimum speed is 60.000, if this gets abused your bot might get IP-banned !');
- }
-// setInterval(changeColor, config.speed);
- //changeColor();
+     console.log(time +`\x1b[33m%s\x1b[0m`, '[WARNING]', '\x1b[0m', 'The recommended speed is 60.000, if you abuse this speed, your robot can be banned from the IP!');
+    }
+    
+    var color = config.color
+    var i = 0;
+    setInterval(function() {
+      client.guilds.cache.get(config.server_ID).roles.cache.get(config.role_ID).setColor(color[parseInt(i, 10)]);
+      if (color[parseInt(i + 1)]) {
+        i++;
+      } else {
+        i = 0;
+      }
+    }, config.speed);
 
 });
-
-client.on('message', message => {
-
-    
-})
 
 client.login(config.token);
